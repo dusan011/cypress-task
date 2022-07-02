6e79d6d5c6b84ab570f8d34131f471c46d99c5ab
Dusan Petrovic <dusan.petrovic@appsolute-mobility.com>
2022-07-02 23:38:58 +0200

    add custom command editCreatedAdvertisement

--- cypress/e2e/ui/advertisement/advertisement.cy.ts
+++ cypress/e2e/ui/advertisement/advertisement.cy.ts
@@ -9,17 +9,20 @@ describe('Create and Edit Advertisement UI Test', ()=>{
         cy.fixture('advertisement').then((advertisementFixture) => {
             advertisement = advertisementFixture;
         });
-        cy.visit("https://admin-advertisement.herokuapp.com/advertisements");
     });
+
+    beforeEach(()=>{
+        cy.visit("https://admin-advertisement.herokuapp.com/advertisements");
+    })
     
     it('should create new advertisement', () => {
        //short function is user to generate random uuid
        let name = advertisement.name+generatedUUID;
        let street = advertisement.street+generatedUUID;
+       console.log("Generated name: "+name);
+       console.log("Generated street: "+street);
+
        cy.createAdvertisement(name, street, advertisement.rooms, advertisement.price);
-       
-       cy.get('[ng-disabled="$ctrl.disableSave"] > .ng-scope').click();
-       cy.get('.md-toast-content').should('contain', 'Saved successfully');
 
        cy.get('advertisement-list md-table-container').should('contain',name);
        cy.get('advertisement-list md-table-container').should('contain',street);
@@ -31,4 +34,24 @@ describe('Create and Edit Advertisement UI Test', ()=>{
        cy.get('#input_7').should('have.value', advertisement.rooms);
        cy.get('#input_8').should('have.value', advertisement.price);
     });
+
+    it('should edit created advertisement', () => {
+        let createdName = advertisement.name+generatedUUID;
+        let editName = "CYEditName"+generatedUUID;
+        let editStreet = "CYEditName"+generatedUUID;
+        let editRooms = "18";
+        let editPrice = "8";
+
+        cy.editCreatedAdvertisement(createdName, editName, editStreet, editRooms, editPrice);
+
+        cy.get('advertisement-list md-table-container').should('contain',editName);
+        cy.get('advertisement-list md-table-container').should('contain',editStreet);
+
+        cy.get('advertisement-list md-table-container').contains(editName).click();
+
+        cy.get('#input_5').should('have.value', editName);
+        cy.get('#input_6').should('have.value', editStreet);
+        cy.get('#input_7').should('have.value', editRooms);
+        cy.get('#input_8').should('have.value', editPrice);
+     });
 })
